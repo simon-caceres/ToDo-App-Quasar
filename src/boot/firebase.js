8590539e135b6bc8c67157fbf2aa12471e0bd64f
simon caceres <kcrs2194@gmail.com>
2020-05-31 16:31:21 -0400

    arreglos de seguridad

--- src/boot/firebase.js
+++ src/boot/firebase.js
@@ -4,13 +4,7 @@ import "firebase/firestore";
 // Agregar configuración firebase:
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAl6YcFPaRoZVrOKeGElOKKyr8_pNncfj0",
-    authDomain: "quasar-firebase-f1e4e.firebaseapp.com",
-    databaseURL: "https://quasar-firebase-f1e4e.firebaseio.com",
-    projectId: "quasar-firebase-f1e4e",
-    storageBucket: "quasar-firebase-f1e4e.appspot.com",
-    messagingSenderId: "215309389259",
-    appId: "1:215309389259:web:8514097c706aa9c0f5ba4e"
+
   };
   // Initialize Firebase
 let firebaseApp = firebase.initializeApp(firebaseConfig);
